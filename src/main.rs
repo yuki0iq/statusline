491b840f3ed076f1ee0930d29f1dd81e494a12e7
Yuki <yuki.utk8g@gmail.com>
2023-07-18 22:34:54 +0300

    add colorized hostname and username with fixed colors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-#![feature(adt_const_params)]
-
+// #![feature(adt_const_params)]
 use const_format::{concatcp, formatcp};
+use std::{env, fs};
 
 const INVISIBLE_START: &str = "\x01";
 const INVISIBLE_END: &str = "\x02";
@@ -45,6 +45,12 @@ macro_rules! rgb {
     };
 }
 
+// TODO compile-time
+fn rgb(r: u8, g: u8, b: u8) -> String {
+    format!("{INVISIBLE_START}{CSI}38;2;{r};{g};{b}m{INVISIBLE_END}")
+}
+
+
 const STYLE_RESET: &str = color!(0u8);
 const STYLE_BOLD: &str = color!(1u8);
 const COLOR_RED: &str = color!(31u8);
@@ -70,6 +76,46 @@ const CURSOR_RESTORE: &str = esc!("[u");
 const CURSOR_UP: &str = esc!("[A");
 const CURSOR_HOME: &str = esc!("[G");
 
+/*
+How to "colorize" a string
+
+using colors table
+   F   C   A   6   0
+ 255 203 153 100   0
+ -> scientific "pick" moment
+
+allow (block too dark and too red)
+FCA60 FCA6 FCA60
+FCA60 0 FCA
+
+block (r06 r00)
+
+total 115 of 5^3 = 125, ban 10 highest
+store in BGR (ban 00r, 06r)
+
+then for hash from 0 to 114
+B = hash / 25
+G = hash / 5 % 5
+R = hash % 5
+
+COLOR_TABLE=(255 203 153 100 0)
+*/
+const COLOR_TABLE: [u8; 5] = [255, 203, 153, 100, 0];
+fn colorize(s: &str) -> String {
+    let hash = usize::from_str_radix(&sha256::digest(format!("{}\n", s))[..4], 16).unwrap() % 115;
+    // println!("{}", &sha256::digest(format!("{}\n", s))[..4]);
+    println!("{}", hash);
+    let b = COLOR_TABLE[hash / 25];
+    let g = COLOR_TABLE[hash / 5 % 5];
+    let r = COLOR_TABLE[hash % 5];
+    format!("{}{s}{STYLE_RESET}", rgb(r, g, b))
+}
+
 fn main() {
-    println!("{COLOR_YELLOW}Hello, world!{STYLE_RESET}");
+    let hostname = fs::read_to_string("/etc/hostname").unwrap_or(String::from("<host>"));
+    let hostname = hostname.trim();
+    let username = env::var("USER").unwrap_or(String::from("<user>"));
+    let workdir = env::var("PWD").unwrap_or(String::new());
+
+    println!("{STYLE_BOLD}{COLOR_YELLOW}(at {}{STYLE_BOLD}{COLOR_YELLOW}){STYLE_RESET} {STYLE_BOLD}{COLOR_BLUE}[as {}{STYLE_BOLD}{COLOR_BLUE}]{STYLE_RESET} -> {}", colorize(&hostname), colorize(&username), colorize(&workdir));
 }
