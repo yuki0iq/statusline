643b1b4d279eacc3c44a5384ec6ea53ccab1177e
Yuki <yuki.utk8g@gmail.com>
2023-07-23 22:01:15 +0300

    fix "get elapsed time" in bash code

--- src/shell.sh
+++ src/shell.sh
@@ -1,12 +1,12 @@
 exec {PS1_FD}<> <(true)
 
-export PS1_START="${EPOCHREALTIME/,/}"
+export PS1_START="${EPOCHREALTIME/[.,]/}"
 
 trap 'echo >&$PS1_FD' DEBUG
-export PS0='${PS1_START:0:$((PS1_START=${EPOCHREALTIME/,/}, 0))}'
+export PS0='${PS1_START:0:$((PS1_START=${EPOCHREALTIME/[.,]/}, 0))}'
 export PROMPT_COMMAND='
     if [[ -n "$PS1_START" ]]; then
-        PS1_END="${EPOCHREALTIME/,/}"
+        PS1_END="${EPOCHREALTIME/[.,]/}"
         PS1_ELAPSED="$((PS1_END - PS1_START))"
         PS1_START=
     else
