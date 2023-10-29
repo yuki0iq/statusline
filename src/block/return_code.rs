aba2ea9c0a80e70972209931d3e4f08545092be9
Yuki <yuki.utk8g@gmail.com>
2023-10-29 07:14:34 +0300

    First version of extended return code information: show signals

--- src/block/return_code.rs
+++ src/block/return_code.rs
@@ -1,8 +1,10 @@
 use crate::{Environment, Icon, IconMode, Pretty, SimpleBlock, Style};
+use nix::{libc, sys::signal::Signal};
 
 pub enum ReturnCode {
     Ok,
-    Failed,
+    Failed(u8),
+    Signaled(u8),
     NotAvailable,
 }
 
@@ -15,8 +17,9 @@ impl SimpleBlock for ReturnCode {
 impl From<&Environment> for ReturnCode {
     fn from(args: &Environment) -> Self {
         match args.ret_code {
-            Some(0) | Some(130) => Self::Ok,
-            Some(_) => Self::Failed,
+            Some(0) => Self::Ok,
+            Some(code) if code < 128 => Self::Failed(code),
+            Some(code) => Self::Signaled(code - 128),
             None => Self::NotAvailable,
         }
     }
@@ -30,9 +33,13 @@ impl Icon for ReturnCode {
                 Text => "OK",
                 Icons | MinimalIcons => "✓",
             },
-            Self::Failed => match &mode {
-                Text => "Failed",
+            Self::Failed(..) => match &mode {
+                Text => "",
                 Icons | MinimalIcons => "✗",
+            },
+            Self::Signaled(..) => match &mode {
+                Text => "SIG",
+                Icons | MinimalIcons => "󰜃 ",
             },
             Self::NotAvailable => match &mode {
                 Text => "N/A",
@@ -44,16 +51,38 @@ impl Icon for ReturnCode {
 
 impl Pretty for ReturnCode {
     fn pretty(&self, mode: &IconMode) -> Option<String> {
-        let icon = self.icon(mode).visible();
+        let icon = self.icon(mode).to_string();
+        let text = match &self {
+            Self::Ok | Self::NotAvailable => icon,
+            // 126 not exec
+            // 127 not found
+            Self::Failed(code) => format!("{code}{icon}"),
+            Self::Signaled(ref sig) => {
+                format!("{icon}{}|{}", signal_name((*sig).into()), 128 + sig)
+            }
+        };
+        let text = text.visible();
+
         Some(
             match &self {
-                Self::Ok => icon.light_green(),
-                Self::Failed => icon.light_red(),
-                Self::NotAvailable => icon.light_gray(),
+                Self::Ok => text.light_green(),
+                Self::Failed(..) => text.light_red(),
+                Self::Signaled(..) => text.true_color(255, 170, 0),
+                Self::NotAvailable => text.light_gray(),
             }
             .with_reset()
             .invisible()
             .to_string(),
         )
+    }
+}
+
+fn signal_name(sig: i32) -> String {
+    if let Ok(sig) = Signal::try_from(sig) {
+        sig.as_str()[3..].to_string()
+    } else if (libc::SIGRTMIN()..=libc::SIGRTMAX()).contains(&sig) {
+        format!("RT{}", sig - libc::SIGRTMIN())
+    } else {
+        String::new()
     }
 }
