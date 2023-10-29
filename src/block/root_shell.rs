87f8b4d78a8766325abe50550853f2e7c57becca
Yuki <yuki.utk8g@gmail.com>
2023-10-29 03:39:01 +0300

    Fix SHLVL display

--- src/block/root_shell.rs
+++ src/block/root_shell.rs
@@ -34,13 +34,14 @@ impl Icon for RootShell {
 
 impl Pretty for RootShell {
     fn pretty(&self, mode: &IconMode) -> Option<String> {
-        let icon = self.icon(mode).visible();
-        let shlvl = if self.1 > 1 {
-            Cow::from(self.1.to_string())
+        let icon = self.icon(mode);
+        let shlvl = if self.1 > 0 {
+            Cow::from((1 + self.1).to_string())
         } else {
             Cow::from("")
         };
         let formatted = format!("{shlvl}{icon}");
+        let formatted = formatted.visible();
         Some(
             if self.0 {
                 formatted.red()
